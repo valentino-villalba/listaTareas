dd01ae2df5361b5acf9f4f58a0bd681bb5963f4a
Valentino Villalba <valentinovillalba61@gmail.com>
2023-12-21 11:56:23 -0300

    agregamos react-bootstrap y bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center text-light display-3">Lista de tareas</h1>
+    </>
+  );
 }
 
 export default App;
